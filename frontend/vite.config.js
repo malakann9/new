8b5df6d75552d02c3feadc8ece15036179079cf4
Ezgi İşgüzar <ezgiisguzar10@gmail.com>
2026-05-26 02:10:39 +0300

    fix(security): add vite-plugin-sri to fix ZAP Sub Resource Integrity missing warnings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sri from 'vite-plugin-sri'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), sri()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
